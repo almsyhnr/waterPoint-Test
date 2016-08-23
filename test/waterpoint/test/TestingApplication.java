fdac5f33541f70363a5d89f39b939a26bed57855
Noor Alamsyah <Noor Alamsyah@DESKTOP-VST5MHJ>
2016-08-23 17:54:00 +0800

    new commit

---
+++ test/waterpoint/test/TestingApplication.java
@@ -0,0 +1,20 @@
+
+
+package waterpoint.test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import waterpoint.test.Application;
+/**
+ *
+ * @author Noor Alamsyah
+ */
+@RunWith(JUnit4.class)
+public class TestingApplication {
+    @Test
+    public void implemented() throws Exception {
+    	Assert.assertTrue("Test to be implemented later".length() > 0);
+    }
+}
